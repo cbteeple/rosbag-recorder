436baebe56c585246724e7b5787b6619daa530de
Michal Slonina <michal.slonina@gmail.com>
2019-10-07 19:46:43 +0100

    convert to python3

--- scripts/rosbag_recorder_server.py
+++ scripts/rosbag_recorder_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 from rosbag_recorder.srv import *
 import rospy
@@ -13,9 +13,9 @@ def recordTopics(req):
 	global pidDict
 
 	command = "rosbag record -b 512 -o " + expanduser("~") + "/" + req.name
-	print "Recording to bag named %s. Topics:"%(req.name)
+	print("Recording to bag named %s. Topics:"%(req.name))
 	for t in req.topics:
-		print t
+		print(t)
 		command += " " + t
 
 	pidDict[req.name] = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd="/tmp/")
@@ -26,14 +26,14 @@ def stopRecording(req):
 	global pidDict
 
 	if req.name in pidDict:
-		print "Stop recording to bag named %s"%(req.name)
+		print("Stop recording to bag named %s"%(req.name))
 		p = pidDict[req.name]
 		process = psutil.Process(p.pid)
 		for subProcess in process.children(recursive=True):
 			subProcess.send_signal(signal.SIGINT)
 		p.wait()
 	else:
-		print "No current recording with name %s!"%req.name
+		print("No current recording with name %s!"%req.name)
 
 	return StopRecordingResponse(True)
  
@@ -41,7 +41,7 @@ def rosbagRecorder():
 	rospy.init_node('rosbag_recorder_server')
 	recordServ = rospy.Service('record_topics', RecordTopics, recordTopics)
 	stopServ = rospy.Service('stop_recording', StopRecording, stopRecording)
-	print "Ready to record topics"
+	print("Ready to record topics")
 	rospy.spin()
 
 if __name__ == "__main__":
